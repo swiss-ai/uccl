e890d2a0696fa0592233846271c993c1b5325431
Andres Nowak <andresnowak52@gmail.com>
2026-05-28 13:58:54 +0200

    Add CXI mode to FIFO benchmark

--- ep/bench/fifo/benchmark.py
+++ ep/bench/fifo/benchmark.py
@@ -7,6 +7,14 @@ parser.add_argument("-l", action="store_true", help="Run latency mode")
 parser.add_argument("-b", action="store_true", help="Run burst mode")
 parser.add_argument("-r", action="store_true", help="Run random mode")
 parser.add_argument("-c", action="store_true", help="Run mops-controlled mode")
+parser.add_argument("-x", "--cxi", action="store_true", help="Run CXI post/completion mode")
+parser.add_argument("--cxi-bytes", type=int, default=8, help="Bytes per CXI write in CXI mode")
+parser.add_argument(
+    "--cxi-max-outstanding",
+    type=int,
+    default=1024,
+    help="Maximum outstanding CXI writes per proxy in CXI mode",
+)
 args = parser.parse_args()
 
 if args.l:
@@ -15,8 +23,17 @@ elif args.b:
     cmd = ["./benchmark", "-b"]
 elif args.r:
     cmd = ["./benchmark", "-r"]
-elif args.e:
+elif args.c:
     cmd = ["./benchmark", "-c"]
+elif args.cxi:
+    cmd = [
+        "./benchmark",
+        "-x",
+        "--cxi-bytes",
+        str(args.cxi_bytes),
+        "--cxi-max-outstanding",
+        str(args.cxi_max_outstanding),
+    ]
 else:
     cmd = ["./benchmark"]
 
